f3f42419f5f3b2d94b950df4d01f0a130c9ecbc9
Tom Nordal <tnordal@gmail.com>
2020-03-28 06:34:16 +0100

    Add live_data_history to get x hours of history

--- dash_app/apps/db.py
+++ dash_app/apps/db.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine, Column, String, Float, DateTime, Integer, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime as dt 
+import datetime as dt 
 import os
+import pandas as pd
 
 
 Base = declarative_base()
@@ -10,7 +11,7 @@ Base = declarative_base()
 class LiveTable(Base):
     __tablename__ = 'livetable'
     id = Column('id', Integer, primary_key=True)
-    timestamp = Column('timestamp', DateTime, default=dt.now)
+    timestamp = Column('timestamp', DateTime, default=dt.datetime.now)
     power = Column('power', Integer)
     min_power = Column('min_power', Float)
     max_power = Column('max_power', Float)
@@ -18,16 +19,19 @@ class LiveTable(Base):
     accumulated = Column('accumulated', Float)
     accumulated_cost = Column('accumulated_cost', Float)
     currency = Column('currency', String(5))
-    
 
-def live_data():
+def get_session():
     db_path = os.path.join(os.path.dirname(__file__), '..', 'tibber_live.db')
     db_path = 'tibber_live.db'
     engine = create_engine('sqlite:///{}'.format(db_path), echo=False)
     Base.metadata.create_all(bind=engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+    return session    
+
+
+def live_data():
+    session = get_session()
     output = {}
     query = session.query(func.max(LiveTable.timestamp).label('live_timestamp')).first()
     query = session.query(LiveTable).filter_by(timestamp = query.live_timestamp).first()
@@ -46,4 +50,28 @@ def live_data():
     session.close()
     return output
 
+def live_data_history():
+    session = get_session()
+    first_date = dt.datetime.today() - dt.timedelta(hours=4)
 
+    query = session.query(LiveTable).filter(LiveTable.timestamp > first_date).all()
+    
+    df_rows = []
+    for q in query:
+        dict1 = {
+            'timestamp':q.timestamp,
+            'power':q.power,
+            'max_power':q.max_power,
+            'min_power':q.min_power,
+            'avg_power':q.avg_power,
+            'accumulated':q.accumulated,
+            'accumulated_cost':q.accumulated_cost,
+            'currency':q.currency
+        }
+        df_rows.append(dict1)
+
+    df = pd.DataFrame(df_rows)
+    return df
+
+if __name__ == "__main__":
+    print(live_data_history().head())
